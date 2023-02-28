908890352863b91efd5d3279856a92eb85a87029
THINK <85819739+thinkorsinkk@users.noreply.github.com>
2023-02-28 15:58:07 +0200

    Delete src directory

--- src/ent.asm
+++
@@ -1,7 +0,0 @@
-global _start
-
-extern kstart
-
-section .text
-_start:
-    jmp kstart
--- src/entry.c
+++
@@ -1,9 +0,0 @@
-__attribute__((__noreturn__)) void hlt() {
-    for (;;) {
-        __asm__("hlt");
-    }
-}
-
-void kstart() {
-    hlt();
-}
